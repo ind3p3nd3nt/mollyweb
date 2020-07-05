1929668daa78cecc1e284ce78921c1d61d52b605
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-07-05 08:35:00 -0400

    Update completed
    Everything normal

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-# You may as well visit https://github.com/independentcod/mollyweb 
-# Apache2 on Debian, Ubuntu or Kali is recommended, throws some config error with HTTPd on CentOS.
+# How to host a website with SSL 
+## Including a template of the Official site of Playboy Model Molly Eskam - https://www.mollyeskam.net
+## (Made by me)
+# Follow Molly Eskam Here:
+## https://www.instagram.com/mollyeskam/
+# Her YouTube Channel:
+## https://www.youtube.com/user/velvetevening1
+## An Apache2 installation on Debian is preferred.
+# HOW TO INSTALL (Pretty simple)
+## git clone https://github.com/independentcod/mollyweb.git && sh mollyweb/bootstrap.sh
+### I made this repository only because of that if I die someday someone can take this website and re-host it.
+### Everything I do I do it for Molly Eskam and the community.
 
 echo ***Install required files***;
 if [ -f "/usr/bin/yum" ]; then
