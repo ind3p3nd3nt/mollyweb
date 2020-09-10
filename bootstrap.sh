4ba7642e108b101a52fea798f76f0edfbb2d3365
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-09 23:40:25 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,6 @@
 ## git clone https://github.com/independentcod/mollyweb.git && sh mollyweb/bootstrap.sh
 ### I made this repository only because of that if I die someday someone can take this website and re-host it.
 ### Everything I do I do it for Molly Eskam and the community.
-
 echo ***Install required files***;
 if [ -f "/usr/bin/yum" ]; then
     sudo yum install epel-release unzip alien mod_ssl httpd php nmap whois -y
@@ -31,23 +30,23 @@ echo ***make a temporary directory***;
 sudo mkdir /root/mollywebsite;
 echo ***change directory***;
 cd mollywebsite;
-echo ***make directory for the lounge live chat plugin***;
-sudo mkdir /etc/thelounge;
-if [ -f "/usr/bin/yum" ]; then
-    echo ***convert thelounge .deb to .rpm for CentOS***;
-    sudo alien -r /var/www/html/thelounge_4.1.0_all.deb
-    echo ***install thelounge .rpm***;
-    sudo rpm -i /var/www/html/thelounge_4.1.0_all.noarch.rpm
-fi
-if [ -f "/usr/bin/apt" ]; then
-    echo ***install thelounge for Debian***;
-    sudo apt install nodejs -y;
-    sudo dpkg -i /var/www/html/thelounge_4.1.0_all.deb
-    echo ***resolving thelounge dependencies.***;
-    sudo apt -f install -y;
-fi
-echo ***move necesary files from temp thelounge directory to program directory***;
-sudo cp -r /var/www/html/thelounge/* /etc/thelounge/;
+#echo ***make directory for the lounge live chat plugin***removed for lack of interests;
+#sudo mkdir /etc/thelounge;
+#if [ -f "/usr/bin/yum" ]; then
+#    echo ***convert thelounge .deb to .rpm for CentOS***;
+#    sudo alien -r /var/www/html/thelounge_4.1.0_all.deb
+#    echo ***install thelounge .rpm***;
+#    sudo rpm -i /var/www/html/thelounge_4.1.0_all.noarch.rpm
+#fi
+#if [ -f "/usr/bin/apt" ]; then
+#    echo ***install thelounge for Debian***;
+#    sudo apt install nodejs -y;
+#    sudo dpkg -i /var/www/html/thelounge_4.1.0_all.deb
+#    echo ***resolving thelounge dependencies.***;
+#    sudo apt -f install -y;
+#fi
+#echo ***move necesary files from temp thelounge directory to program directory***;
+#sudo cp -r /var/www/html/thelounge/* /etc/thelounge/;
 echo ***move the config file into the correct OS directory***;
 if [ -f "/usr/bin/yum" ]; then
 sudo cp -r /var/www/html/httpd.conf /etc/httpd/conf/;
@@ -59,7 +58,7 @@ echo ***Give permissions***;
 sudo chmod +rwx /etc/thelounge -R;
 sudo chmod +rwx /var/www/html -R;
 echo ***Activate SSL***;
-a2enmod ssl;
+sudo a2enmod ssl;
 cd /etc/apache2;
 echo ***Get some config files needed***;
 sudo wget -O sites-enabled/000-default.conf https://archive.org/download/mollywebsite.tar/000-default.conf;
@@ -73,5 +72,5 @@ sudo wget -O /root/mollywebsite/cert.zip https://archive.org/download/mollywebsi
 echo ***Decompress certs***;
 cd /etc/letsencrypt/live/www.mollyeskam.net;
 sudo unzip /root/mollywebsite/cert.zip;
-sudo systemctl enable apache2;
+#sudo systemctl enable apache2;
 echo "Setup complete, to start webserver type sudo apache2 && sudo thelounge start&";
